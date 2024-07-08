ed9c656eaf644ffa825e584f40be132be5b2b502
Shimul Hossain <shimultechtips@gmail.com>
2024-07-08 20:07:09 +0600

    09-B-07

--- config/authGoogleConfig.js
+++ config/authGoogleConfig.js
@@ -7,7 +7,12 @@ const strategy = new GoogleStrategy(
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3001/auth/google/redirect",
   },
-  () => {}
+  (accessToken, refreshToken, profile, cb) => {
+    console.log("I Am Callback Function!");
+    console.log("Profile : ", profile._json);
+
+    cb();
+  }
 );
 
 passport.use(strategy);
--- routers/authGoogleRouter.js
+++ routers/authGoogleRouter.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 const passport = require("passport");
 require("../config/authGoogleConfig");
 
-router.route("/").get(passport.authenticate("google", { scope: ["profile"] }));
+router
+  .route("/")
+  .get(passport.authenticate("google", { scope: ["profile", "email"] }));
 
-router.route("/redirect").get();
+router.route("/redirect").get(
+  passport.authenticate("google", (req, res) => {
+    console.log(req.body);
+    res.send("I Am Redirect URI!");
+  })
+);
 
 module.exports = router;
