8eb03565a4353b04f9135030cd504702927cd3fd
Shimul Hossain <shimultechtips@gmail.com>
2024-07-08 17:48:53 +0600

    09-A-01

---
+++ server.js
@@ -0,0 +1,14 @@
+required("dotenv/config");
+const app = require("./app");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected To MongoDB"))
+  .catch((err) => console.error("Mongodb Connection Failed"));
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`App Running On Port ${port}!`);
+});
